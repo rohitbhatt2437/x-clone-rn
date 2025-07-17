c454dc8d4ed87f438ce2deddbecf7bf93e2dda88
Rohit Bhatt <bhattrohed@gmail.com>
2025-07-17 18:54:49 +0530

    db models created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, ()=>{
+connectDB();
+
+app.get('/', (req, res)=>{
+    res.send("center")
+})
+
+app.listen(ENV.PORT, ()=>{
     console.log("Server running on port 5001")
 })
